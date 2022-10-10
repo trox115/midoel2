762e4acbe1f31de604544aac1507848270f7f0c8
trox115 <tofernandes67@gmail.com>
2022-10-10 17:28:33 +0100

    test new api changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ const Home = ({ header, image, products, featured, footerData }) => {
   return (
     <>
       <Header logo={header.siteLogoUrl} items={header.headerMenuItems} siteDescription={header.siteDescription} siteLogo={header.siteLogo} siteTitle={header.siteTitle} />
-      <HomePage image={ image } newProducts={products} featured={ featured }/>
+      <HomePage image={ image } newProducts={ products} featured={ featured } />
       <Footer footerData={footerData} />
     </>
   )
@@ -35,14 +35,17 @@ export async function getStaticProps(context) {
   const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer`);
   const newData = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/post?post_id=48`);
   const { data : footerData } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/posts-by-tax?post_type=post&taxonomy=category&slug=footer`);
+  const { data :newProducts } = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/get-new`);
+   const { data :featured } = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/get-fetured`);
+   console.log(newProducts);
   return {
     props: {
       header: data.data.header || {},
       image: newData.data.post_data.attachment_image.img_src[0] || {},
-      // products: newProducts.products,
-      // featured: featured.products,
+      products: newProducts.products,
+      featured: featured.products,
       footerData: {...footerData.data, favIcon:data.data.header.favicon },
-      revalidate: 1
+      revalidate: 60
     }
   }
 }
