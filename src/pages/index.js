a729d14cfaceeb96a647f62e50564c8ea622ec49
trox115 <tofernandes67@gmail.com>
2022-10-10 17:00:46 +0100

    update api calls

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,19 +15,19 @@ const Home = ({ header, image, products, featured, footerData }) => {
 }
 
 export const getServerSideProps = async (context) => {
-  const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer`);
-  const newData = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/post?post_id=48`);
-  const { data : footerData } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/posts-by-tax?post_type=post&taxonomy=category&slug=footer`);
+  // const { data } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/header-footer?header_location_id=hcms-menu-header&footer_location_id=hcms-menu-footer`);
+  // const newData = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/post?post_id=48`);
+  // const { data : footerData } = await axios.get(`${process.env.NEXT_PUBLIC_URL}/wp-json/rae/v1/posts-by-tax?post_type=post&taxonomy=category&slug=footer`);
   const { data :newProducts } = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/get-new`);
   const { data :featured } = await axios.get(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/get-fetured`);
 
   return {
     props: {
-      header: data.data.header || {},
-      image: newData.data.post_data.attachment_image.img_src[0] || {},
+      // header: data.data.header || {},
+      // image: newData.data.post_data.attachment_image.img_src[0] || {},
       products: newProducts.products,
       featured: featured.products,
-      footerData: {...footerData.data, favIcon:data.data.header.favicon },
+      // footerData: {...footerData.data, favIcon:data.data.header.favicon },
       revalidate: 1
     }
   };
